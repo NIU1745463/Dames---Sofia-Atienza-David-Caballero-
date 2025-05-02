c882f0a0e71af9e1bdb91f6e162dfa74677a6191
sofiatien <1708916@uab.cat>
2025-05-02 17:33:28 +0200

    Update tauler.hpp

--- Projecte - Dames (David, Sofia)/Projecte - Dames (David, Sofia)/tauler.hpp
+++ Projecte - Dames (David, Sofia)/Projecte - Dames (David, Sofia)/tauler.hpp
@@ -14,30 +14,27 @@ const int MAX_MOVIMENTS = 1000;
 class Moviment;
 
 class Tauler {
-public:
+public: 
+    //CONSTRUCTOR
     Tauler();
-    friend class Moviment;
+    friend class Moviment; 
     void inicialitza(const string& nomFitxer);
     void actualitzaMovimentsValids();
     void getPosicionsPossibles(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[]) const;
     bool mouFitxa(const Posicio& origen, const Posicio& desti);
-    //CREAR PARA BUFAR O IMPLEMENTAR EN BUFAR bool maximaCaptura(const Posicio& origen) const;
-    //hacefalta?void aplicarMoviment(const Moviment& moviment);
-
     void bufarFitxa(ColorFitxa color);
     string toString() const;
+
 private:
     Fitxa m_tauler[N_FILES][N_COLUMNES];
     void netejaTauler();
     void llegeixTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUMNES]);
     void escriuTauler(const string& nomFitxer, char tauler[N_FILES][N_COLUMNES]);
-    //crear esta en la funcion del tauler void generarMovimentsFitxaNormal(const Posicio& pos, Moviment& mov) const;
-    // lo mismo aquí void generarMovimentsDama(const Posicio& pos, Moviment& mov) const;
     bool dinsTauler(const Posicio& pos) const;
     bool casellaBuida(const Posicio& pos) const;
     bool fitxaContraria(const Posicio& pos, ColorFitxa color) const;
     void buscarCaptures(const Posicio& origen, int& nPosicions, Posicio posicionsPossibles[]) const;
-    string m_movimentsValids[100]; //supongo que lo tendremos que hacer dinamico
+    string m_movimentsValids[100]; //més endavant serà dinamic
     int m_numMoviments;
     int calculaNumCaptures(const Posicio& origen, const Posicio& desti) const;
 
